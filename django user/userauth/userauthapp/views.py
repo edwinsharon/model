3a869f3f1054a7d7a6924618a5817d54596a406a
edwinsharon <edwinachayan@gmail.com>
2024-06-10 15:43:26 +0530

    done

--- django user/userauth/userauthapp/views.py
+++ django user/userauth/userauthapp/views.py
@@ -8,7 +8,7 @@ def index(request):
         username=request.POST['username']
         password=request.POST['password']
         user=User.objects.create_user(username=username,password=password)
-        return render(request,"main.html",{'user':user})
+        return render(request,"success.html",{'user':user})
     return render(request,"index.html")
 def main(request):
     return render(request,"main.html")    
@@ -19,7 +19,7 @@ def loginin(request):
       password=request.POST['password']
       user=authenticate(username=username,password=password)
       if user is not None:
-          return render(request,"main.html",{"user":user})
+          return render(request,"success.html",{"user":user})
       else:
           return render(request,"login.html")
     return render(request,"login.html")    
