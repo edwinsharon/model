a95d0c894521bcff75d11af9d62d6ddb537d6f41
edwinsharon <edwinachayan@gmail.com>
2024-07-18 11:59:17 +0530

    done

--- ecomweb/ecomapp/views.py
+++ ecomweb/ecomapp/views.py
@@ -12,6 +12,17 @@ import random
 
 # Create your views here.
 def index(request):
+    if request.POST:
+        category=request.POST.get('category')
+        if category=="all":
+            products = product.objects.all()
+            return render(request,'index.html',{'products': products})
+        else:    
+            products = product.objects.filter(category=category)
+            return render(request,'index.html',{'products': products})
+
+       
+
     products = product.objects.all()
     return render(request,'index.html',{'products': products})
 def createseller(request):
